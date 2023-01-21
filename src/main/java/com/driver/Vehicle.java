a0d6c2f158532c3817e226d912bc61f3d150a466
Tharun <tharunkumarreddy1212@gmail.com>
2023-01-21 18:53:27 +0530

    Solution

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,23 +6,55 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
-    public Vehicle(String name) {
 
+
+    public Vehicle(String name)
+    {
+       this.name=name;
+       this.currentSpeed=0;
+       this.currentDirection=0;
     }
 
-    public void steer(int direction){
-        //direction is in degrees, add it to the current direction
-        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    public void move(int speed,int direction)
+    {
+        currentSpeed=speed;
+        currentDirection=direction;
+        System.out.println("Speed Changed to:"+currentSpeed+ ",and the direction is changed to:" +currentDirection+ " degrees");
     }
 
-    public void move(int speed, int direction){
-        //set the values of currentSpeed and currentDirection
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
+    public void steer(int direction)
+    {
+        currentDirection+=direction;
+        System.out.println("The direction change :" + currentDirection + " degrees");
     }
 
     public void stop(){
-
-        System.out.println("stop method called - The vehicle is stopped");
+        //direction is in degrees, add it to the current direction
+        currentSpeed=0;
+        System.out.println("The Vehicle Stopped");
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
 }
